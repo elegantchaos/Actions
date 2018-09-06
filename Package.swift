486913e31b75737598b1e2c8271603bd41de1b82
Sam Deane <sam@elegantchaos.com>
2018-09-06 18:05:40 +0100

    Use https for dependencies.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Actions"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:elegantchaos/Logger", from: "1.0.11"),
+        .package(url: "https://github.com/elegantchaos/Logger", from: "1.0.11"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
