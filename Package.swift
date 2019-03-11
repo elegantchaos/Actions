954979360f261a389e5d325f8649f34f5e835b99
Sam Deane <sam@elegantchaos.com>
2019-03-11 12:59:00 +0000

    added coverage script

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.2"),
+        .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
