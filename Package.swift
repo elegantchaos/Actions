1055884edc8cf69c5ba80b8c9ffd174bcd28f6b8
Sam Deane <sam@elegantchaos.com>
2019-06-19 12:38:07 +0100

    Updated dependencies. Required macOS 10.13.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Actions",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,8 +18,8 @@ let package = Package(
             targets: ["ActionsKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.3.5"),
-        .package(url: "https://github.com/elegantchaos/Coverage.git", from: "1.0.5"),
+        .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.3.6"),
+        .package(url: "https://github.com/elegantchaos/Coverage.git", from: "1.0.6"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
